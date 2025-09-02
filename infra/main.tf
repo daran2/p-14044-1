6d85d842008b3db60e0dfd23581b858e3969d6e2
darancode <darancode@gmail.com>
2025-09-03 08:40:55 +0900

    creating varables.tf

--- infra/main.tf
+++ infra/main.tf
@@ -9,7 +9,7 @@ terraform {
 
 # AWS 설정 시작
 provider "aws" {
-  region = "ap-northeast-2"
+  region = var.region // 테라폼은 기본적으로 같은 폴더에 있는 파일이 import돼서 var로 쓸 수 있음
 }
 # AWS 설정 끝
 
@@ -21,6 +21,21 @@ resource "aws_vpc" "vpc_1" {
   enable_dns_hostnames = true
 
   tags = {
-    Name = "terra-vpc-1"
+    Name = "${var.prefix}-vpc-1"
   }
+
+  
+}
+
+resource "aws_vpc" "vpc_2" {
+  cidr_block = "10.0.0.0/16"
+
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "${var.prefix}-vpc-2"
+  }
+
+  
 }
